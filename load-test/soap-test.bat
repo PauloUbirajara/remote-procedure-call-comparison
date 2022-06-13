68f0a1b519756804e37c960ae8875b204da9dae9
Paulo Ubirajara de Mattos Neto <ubirajara3@hotmail.com>
2022-06-13 02:56:21 -0300

    Update soap-test.bat

--- load-test/soap-test.bat
+++ load-test/soap-test.bat
@@ -6,12 +6,12 @@ k6 run .\soap.js  -u 100 --summary-export=soapResults/soap3.json -e LOADPROFILE=
 
 k6 run .\soap.js  -u 10 --summary-export=soapResults/soap4.json -e LOADPROFILE=media
 
-k6 run .\soap.js  -u 100 --summary-export=soapResults/soap5.json -e LOADPROFILE=media
+k6 run .\soap.js  -u 50 --summary-export=soapResults/soap5.json -e LOADPROFILE=media
 
 k6 run .\soap.js  -u 100 --summary-export=soapResults/soap6.json -e LOADPROFILE=media
 
 k6 run .\soap.js  -u 10 --summary-export=soapResults/soap7.json -e LOADPROFILE=alta
 
-k6 run .\soap.js  -u 100 --summary-export=soapResults/soap8.json -e LOADPROFILE=alta
+k6 run .\soap.js  -u 50 --summary-export=soapResults/soap8.json -e LOADPROFILE=alta
 
 k6 run .\soap.js  -u 100 --summary-export=soapResults/soap9.json -e LOADPROFILE=alta
