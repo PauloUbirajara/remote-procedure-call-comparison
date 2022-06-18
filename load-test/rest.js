1743110da5eb776fa50aeafb3c02acd224285611
PauloUbirajara <ubirajara3@hotmail.com>
2022-06-18 15:10:44 -0300

    Ajustar rotas REST

--- load-test/rest.js
+++ load-test/rest.js
@@ -1,72 +1,75 @@
-import http from 'k6/http'
-import { check, sleep } from 'k6'
+import http from 'k6/http';
+import { check, sleep } from 'k6';
 
-const load = __ENV.LOADPROFILE
-const maxUsers = __ENV.MAX_USERS
+const load = __ENV.LOADPROFILE;
+const maxUsers = __ENV.MAX_USERS;
 
 export const options = {
 	vus: maxUsers, // Número máximo de usuário concorrentes
 	duration: '3m' // Duração máxima do teste (pode ser que o teste termine mais cedo devido o limite de iterações)
-}
+};
+
+const PORT = 5000;
+const baseURL = `http://localhost:${PORT}`;
 
 function cargaBaixa() {
-	const responseUsers = http.get('http://localhost:3000/users')
+	const responseUsers = http.get(`${baseURL}/users`);
 
 	check(responseUsers, {
 		'responseUsers OK': (r) => {
-			return r && r.status === 200
+			return r && r.status === 200;
 		}
-	})
+	});
 }
 
 function cargaMedia() {
-	const responseUsers = http.get('http://localhost:3000/users')
-	const responsePlaylists = http.get('http://localhost:3000/playlists')
+	const responseUsers = http.get(`${baseURL}/users`);
+	const responsePlaylists = http.get(`${baseURL}/playlists`);
 
 	check(responseUsers, {
 		'responseUsers OK': (r) => {
-			return r && r.status === 200
+			return r && r.status === 200;
 		}
-	})
+	});
 
 	check(responsePlaylists, {
 		'responsePlaylists OK': (r) => {
-			return r && r.status === 200
+			return r && r.status === 200;
 		}
-	})
+	});
 }
 
 function cargaAlta() {
-	const responseUsers = http.get('http://localhost:3000/users')
-	const responsePlaylists = http.get('http://localhost:3000/playlists')
-	const responseSongs = http.get('http://localhost:3000/songs')
+	const responseUsers = http.get(`${baseURL}/users`);
+	const responsePlaylists = http.get(`${baseURL}/playlists`);
+	const responseSongs = http.get(`${baseURL}/songs`);
 
 	check(responseUsers, {
 		'responseUsers OK': (r) => {
-			return r && r.status === 200
+			return r && r.status === 200;
 		}
-	})
+	});
 
 	check(responsePlaylists, {
 		'responsePlaylists OK': (r) => {
-			return r && r.status === 200
+			return r && r.status === 200;
 		}
-	})
+	});
 
 	check(responseSongs, {
 		'responseSongs OK': (r) => {
-			return r && r.status === 200
+			return r && r.status === 200;
 		}
-	})
+	});
 }
 
 const cargas = {
 	baixa: cargaBaixa,
 	media: cargaMedia,
-	alta: cargaMedia
-}
+	alta: cargaAlta
+};
 
 export default () => {
-	cargas[load]()
-	sleep(1)
-}
+	cargas[load]();
+	sleep(1);
+};
